7e515130ef401f88aadaf4cdc91cfc67740ceb73
mohan <64525016+Infinity-code@users.noreply.github.com>
2024-04-26 01:09:04 +0530

    Update weather_data.tsx

--- _components/weather_data/weather_data.tsx
+++ _components/weather_data/weather_data.tsx
@@ -92,7 +92,7 @@ import "./weather.css"
     const bgSet=DayNite({time:sunUpdate,sun:DaySun,moon:NightMoon}).type.render.displayName==="Moon"?bg[0].night:bg[0].day;
     console.log(bgSet[1]);
     return(
-        <div className={`w-screen h-screen bg-gradient-to-t ${bgSet[0]} sky grid grid-rows-10` }>
+        <div className={`w-screen h-screen bg-gradient-to-t ${bgSet[0]} grid grid-rows-10` }>
         <div className="row-span-9 grid grid-cols-4 ">
           <div className="col-span-3 ">
             <div className="flex justify-start row-span-1 ">
